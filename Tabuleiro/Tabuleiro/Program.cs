eb945697cd270b5ad5fec71f2d08a05e9d7cf671
Mateus Melo Oliveira <Mateus99Melo@gmail.com>
2022-09-06 11:40:28 -0300

    Commit inicial

---
+++ Tabuleiro/Tabuleiro/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Tabuleiro {
+    class Program {
+        static void Main(string[] args) {
+            for (int i = 0; i < 3; i++) {
+                Console.WriteLine("Qual a dimensão do tabuleiro?");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine((n * n) - ((n * n) / 2) + " Casas Brancas E " + ((n * n) / 2) + " Casas Pretas ");
+            }
+        }
+    }
+}
